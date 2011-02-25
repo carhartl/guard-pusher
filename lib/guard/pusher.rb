36a6e1c9b9bb311cedacdf2926a2f910a97dc62b
Klaus Hartl <klaus.hartl@gmail.com>
2011-02-25 17:37:50 +0100

    fixing module lookup and Pusher setup

--- lib/guard/pusher.rb
+++ lib/guard/pusher.rb
@@ -1,18 +1,19 @@
 require 'guard'
 require 'guard/guard'
+require 'pusher'
 
 module Guard
   class Pusher < Guard
 
     def initialize(watchers = [], options = {})
       super
-      Pusher.app_id = options['app_id'] if Pusher.app_id.nil?
-      Pusher.key = options['key'] if Pusher.key.nil?
-      Pusher.secret = options['secret'] if Pusher.secret.nil?
+      ::Pusher.app_id = options[:app_id] if ::Pusher.app_id.nil?
+      ::Pusher.key = options[:key] if ::Pusher.key.nil?
+      ::Pusher.secret = options[:secret] if ::Pusher.secret.nil?
     end
 
     def run_on_change(paths)
-      Pusher['guard-pusher'].trigger('reload', {})
+      ::Pusher['guard-pusher'].trigger('reload', {})
     end
 
   end
