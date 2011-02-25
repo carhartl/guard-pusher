2f917dd129ec86b703df1f96b0f3aed9ac00fc61
Klaus Hartl <klaus.hartl@gmail.com>
2011-02-25 23:25:51 +0100

    adding missing specs

--- spec/guard/pusher_spec.rb
+++ spec/guard/pusher_spec.rb
@@ -1,7 +1,45 @@
 require 'spec_helper'
 
-describe "GuardPusher" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Guard::Pusher do
+
+  describe "options" do
+    context "Pusher not configured" do
+      it "requires 'app_id', 'key' and 'secret'" do
+        Pusher.should_receive(:app_id=).with('1234')
+        Pusher.should_receive(:key=).with('key')
+        Pusher.should_receive(:secret=).with('secret')
+        Guard::Pusher.new([], {
+          :app_id => '1234',
+          :key    => 'key',
+          :secret => 'secret'
+        })
+      end
+    end
+
+    context "Pusher already configured" do
+      it "does not require 'app_id', 'key' and 'secret'" do
+        Pusher.app_id = "1234"
+        Pusher.key = "key"
+        Pusher.secret = "secret"
+        Pusher.should_not_receive(:app_id=)
+        Pusher.should_not_receive(:key=)
+        Pusher.should_not_receive(:secret=)
+        Guard::Pusher.new([], {
+          :app_id => '1234',
+          :key    => 'key',
+          :secret => 'secret'
+        })
+      end
+    end
   end
+
+  describe "run_on_change" do
+    it "sends Pusher message" do
+      channel = mock(Pusher::Channel)
+      Pusher.should_receive(:[]).with('guard-pusher').and_return(channel)
+      channel.should_receive(:trigger).with('reload', {})
+      subject.run_on_change(['foo'])
+    end
+  end
+
 end
