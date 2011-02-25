a9cdd557fdf1f579bcb3c27e9239fb737ac67135
Klaus Hartl <klaus.hartl@gmail.com>
2011-02-25 15:42:54 +0100

    initial

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source "http://rubygems.org"
 # Add dependencies required to use your gem here.
 # Example:
 #   gem "activesupport", ">= 2.3.5"
+gem "guard", ">= 0.3"
+gem "pusher", ">= 0.8"
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
---
+++ lib/guard/pusher.rb
@@ -0,0 +1,19 @@
+require 'guard'
+require 'guard/guard'
+
+module Guard
+  class GuardPusher < Guard
+
+    def initialize(watchers = [], options = {})
+      super
+      Pusher.app_id = options['app_id'] if Pusher.app_id.nil?
+      Pusher.key = options['key'] if Pusher.key.nil?
+      Pusher.secret = options['secret'] if Pusher.secret.nil?
+    end
+
+    def run_on_change(paths)
+      Pusher['guard-pusher'].trigger('reload', {})
+    end
+
+  end
+end
