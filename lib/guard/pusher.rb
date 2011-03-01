07ebc1f90285fa9f691e638404733360313224e5
Klaus Hartl <klaus.hartl@gmail.com>
2011-03-01 16:38:35 +0100

    try loading config from yml file before resorting to options, event name as option

--- lib/guard/pusher.rb
+++ lib/guard/pusher.rb
@@ -7,13 +7,35 @@ module Guard
 
     def initialize(watchers = [], options = {})
       super
-      [ :app_id, :key, :secret ].each { |attr|
-        ::Pusher.send("#{attr}=", options[attr]) unless ::Pusher.send("#{attr}")
-      }
+
+      @options = options
+
+      config = if File.file?('config/pusher.yml')
+        YAML.load_file('config/pusher.yml')['development']
+      else
+        options
+      end
+
+      if config_pusher_with(config)
+        UI.info('Pusher is ready.', :reset => true)
+      else
+        UI.info("D'oh! Pusher not properly configured. Make sure to add app_id, key and secret.", :reset => true)
+      end
     end
 
     def run_on_change(paths)
-      ::Pusher['guard-pusher'].trigger('reload', {})
+      ::Pusher['guard-pusher'].trigger(@options[:event] || 'guard', {})
+    end
+
+
+    private
+
+    def config_pusher_with(credentials)
+      %w{app_id key secret}.each do |config|
+        return false unless (value = credentials[config] || credentials[config.to_sym])
+        ::Pusher.send("#{config}=", value)
+      end
+      true
     end
 
   end
--- spec/guard/pusher_spec.rb
+++ spec/guard/pusher_spec.rb
@@ -2,32 +2,60 @@ require 'spec_helper'
 
 describe Guard::Pusher do
 
-  describe "options" do
-    context "Pusher not configured" do
+  describe "configuration" do
+    context "provided through YAML file" do
+      before(:each) do
+        File.should_receive(:file?).
+          with('config/pusher.yml').
+          and_return(true)
+
+        YAML.should_receive(:load_file).
+          with('config/pusher.yml').
+          and_return({ "development" => { "app_id" => 42, "key" => "fake_key", "secret" => "fake_secret" }})
+      end
+
       it "requires 'app_id', 'key' and 'secret'" do
-        Pusher.should_receive(:app_id=).with('1234')
-        Pusher.should_receive(:key=).with('key')
-        Pusher.should_receive(:secret=).with('secret')
+        Pusher.should_receive(:app_id=).with(42)
+        Pusher.should_receive(:key=).with('fake_key')
+        Pusher.should_receive(:secret=).with('fake_secret')
+        Guard::UI.should_receive(:info).with(/.*Pusher is ready.*/, :reset => true)
+        Guard::Pusher.new([])
+      end
+    end
+
+    context "provided through options" do
+      before(:each) do
+        File.should_receive(:file?).
+          with('config/pusher.yml').
+          and_return(false)
+      end
+
+      it "requires 'app_id', 'key' and 'secret'" do
+        Pusher.should_receive(:app_id=).with(42)
+        Pusher.should_receive(:key=).with('fake_key')
+        Pusher.should_receive(:secret=).with('fake_secret')
+        Guard::UI.should_receive(:info).with(/.*Pusher is ready.*/, :reset => true)
         Guard::Pusher.new([], {
-          :app_id => '1234',
-          :key    => 'key',
-          :secret => 'secret'
+          :app_id => 42,
+          :key    => 'fake_key',
+          :secret => 'fake_secret'
         })
       end
     end
 
-    context "Pusher already configured" do
-      it "does not require 'app_id', 'key' and 'secret'" do
-        Pusher.app_id = "1234"
-        Pusher.key = "key"
-        Pusher.secret = "secret"
+    context "missing the necessery keys" do
+      before(:each) do
+        File.should_receive(:file?).
+          with('config/pusher.yml').
+          and_return(false)
+      end
+
+      it "does not attempt to configure Pusher and issues a warning" do
         Pusher.should_not_receive(:app_id=)
-        Pusher.should_not_receive(:key=)
-        Pusher.should_not_receive(:secret=)
+        Guard::UI.should_receive(:info).with(/.*Pusher not properly configured.*/, :reset => true)
         Guard::Pusher.new([], {
-          :app_id => '1234',
-          :key    => 'key',
-          :secret => 'secret'
+          :key    => 'fake_key',
+          :secret => 'fake_secret'
         })
       end
     end
@@ -37,7 +65,7 @@ describe Guard::Pusher do
     it "sends Pusher message" do
       channel = mock(Pusher::Channel)
       Pusher.should_receive(:[]).with('guard-pusher').and_return(channel)
-      channel.should_receive(:trigger).with('reload', {})
+      channel.should_receive(:trigger).with('guard', {})
       subject.run_on_change(['foo'])
     end
   end
